0621531cd6694dea1aaf7be749ff8dd896675a32
yobgmin <lym9613@police.ac.kr>
2017-11-21 22:31:40 +0900

    dsf

--- pro.php
+++ pro.php
@@ -504,6 +504,7 @@ echo "RemotePwdump";
 echo "<table border=1>";
  	for($x=0;$x<mysqli_num_rows($query_remotepwdump);$x++){
 $data[$x]=mysqli_fetch_array($query_remotepwdump);
+echo $data;
 }
 echo "<tr><td>SourceImage</td><td>hostname</td><td>Time</td></tr>";
 for($x=0;$x<mysqli_num_rows($query_remotepwdump);$x++){
