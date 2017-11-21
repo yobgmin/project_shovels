9809d21c8c4964449b7a3c04ce241a2a38678a07
yobgmin <lym9613@police.ac.kr>
2017-11-21 22:58:44 +0900

    fsdfsd

--- pro.php
+++ pro.php
@@ -525,8 +525,8 @@ $data[$x]=mysqli_fetch_array($query_remotepwdump);
 echo "<tr><td>SourceImage</td><td>hostname</td><td>Time</td></tr>";
 for($x=0;$x<mysqli_num_rows($query_remotepwdump);$x++){
 
-	$query_remotetime=mysqli_query($server,"SELECT * from pipe_created_tbl where EventTime like '".$data[$x][EventTime]."';");
-	//echo mysqli_fetch_array($query_remotettime);
+	$query_remotetime=mysqli_query($server,"SELECT * from pipe_created_tbl where EventTime like '".$data[$x][EventTime]."';";
+	echo mysqli_fetch_array($query_remotetime);
 	$Image22[]=data[$x][SourceImage];
 	$hname22[]=data[$x][Hostname];
 	$EventTime22[]=data[$x][EventTime];
