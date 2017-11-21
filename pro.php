8dee7e3e0445db8acab6678a44f11ff8cca253a1
yobgmin <lym9613@police.ac.kr>
2017-11-21 22:49:55 +0900

    fsdfsd

--- pro.php
+++ pro.php
@@ -520,7 +520,8 @@ echo "RemotePwdump";
 echo "<table border=1>";
  	for($x=0;$x<mysqli_num_rows($query_remotepwdump);$x++){
 $data[$x]=mysqli_fetch_array($query_remotepwdump);
-echo $data[$x][EventTime];
+$EventTime=$data[$x][EventTime];
+echo $EventTime;
 }
 
 echo "<tr><td>SourceImage</td><td>hostname</td><td>Time</td></tr>";
