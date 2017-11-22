298099ccb6aae9f19f294fb82b9f5f7776938d29
yobgmin <lym9613@police.ac.kr>
2017-11-22 21:44:06 +0900

    fsd

--- pro.php
+++ pro.php
@@ -527,7 +527,7 @@ for($x=0;$x<mysqli_num_rows($query_winrs3);$x++){
 	$data[$x]=mysqli_fetch_array($query_winrs3);
 }
 echo "<tr><td>Image</td><td>SourceIP</td><td>DestinationIP</td><td>EventTime</td></tr>";
-//echo "SELECT * from proc_create_tbl where EventTime like '".$data[0][EventTime]."';"
+echo "SELECT * from proc_create_tbl where EventTime like '".$data[$x][EventTime];
 for($x=0;$x<mysqli_num_rows($query_remotepwdump);$x++){
 	$query_remoteproc=mysqli_query($server,"SELECT * from proc_create_tbl where EventTime like '".$data[$x][EventTime]."';");
 	for($y=0;$y<mysqli_num_rows($query_remoteproc);$y++){
