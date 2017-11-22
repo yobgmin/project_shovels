279d48fd144c541db2e62884e7c8f4caf4c2eb41
yobgmin <lym9613@police.ac.kr>
2017-11-23 00:52:42 +0900

    d

--- pro.php
+++ pro.php
@@ -532,7 +532,7 @@ for($x=0;$x<mysqli_num_rows($query_winrs3);$x++){
 echo "<tr><td>Image</td><td>SourceIP</td><td>DestinationIP</td><td>EventTime</td></tr>";
 for($x=0;$x<mysqli_num_rows($query_winrs3);$x++){
 	//echo "SELECT * from proc_create_tbl where EventTime like '".(string)$EventTime[$x]."';";
-	$query_remoteproc=mysqli_query($server,"SELECT * from proc_create_tbl where EventTime like '".(string)$EventTime[$x]."';");
+	//$query_remoteproc=mysqli_query($server,"SELECT * from proc_create_tbl where EventTime like '".(string)$EventTime[$x]."';");
 	/*
 	for($y=0;$y<mysqli_num_rows($query_remoteproc);$y++){
 		$Temp[$y]=mysqli_fetch_array($query_remoteproc);
@@ -548,7 +548,7 @@ for($x=0;$x<mysqli_num_rows($query_winrs3);$x++){
 	$SourceIP23[]=$data[$x][SourceIP];
 	$EventTime23[]=$data[$x][EventTime];
 	$DestinationIP23[]=$data[$x][DestinationIP];
-	echo "<tr><td>$Image23[$x]</td><td>$EventTime23[$x]</td><td>$SourceIP23[$x]</td><td>$DestinationIP23[$x]</td></tr>";
+	echo "<tr><td>$Image23[$x]</td><td>$EventTime[$x]</td><td>$SourceIP23[$x]</td><td>$DestinationIP23[$x]</td></tr>";
 }
 echo "</table>";
 
