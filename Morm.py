18fba8a2c88e88bd848df4ddecf381f3fa1665c0
yobgmin <lym9613@police.ac.kr>
2017-11-27 13:49:26 +0900

    fds

--- Morm.py
+++ Morm.py
@@ -326,4 +326,4 @@ for in1 in Intell1:
 #	print i.ProcessID,i.Image
 
 for i in session.query(raw_access_read_tbl).filter(raw_access_read_tbl.Image.like('%Everything.exe')).filter(raw_access_read_tbl.Image.like('System')).filter(raw_access_read_tbl.Image.like('%System32%')).filter(raw_access_read_tbl.Image.like('%TrustedInstaller.exe')):
-	print i.Image, i.EventTime, i.Hostname
+	print i.ProcessID, i.Image, i.EventTime, i.Hostname
