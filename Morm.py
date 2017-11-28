033425711f0effe27286545f88bf00c70e2e0254
yobgmin <lym9613@police.ac.kr>
2017-11-28 19:08:19 +0900

    fsd

--- Morm.py
+++ Morm.py
@@ -330,6 +330,9 @@ for i in session.query(file_create_tbl).filter(~file_create_tbl.TargetFilename.l
 		print "Execution After Copy", j.ParentProcessId, j.Image, j.EventTime, j.Hostname, j.CommandLine
 		for k in session.query(network_connect_tbl).filter(network_connect_tbl.EventTime.like(i.EventTime)):
 			print "ParentImage Network", k.Image, k.EventTime, k.Hostname, k.DestinationHostname
+		print k
+		for p in session.query(proc_tbl).filter(proc_tbl.EventID.like('1')).filter(proc_tbl.EventTime.like(i.EventTime)):
+			print "Execution from Host", p.ParentProcessId, p.Image, p.EventTime, p.Hostname, p.CommandLine
 """
 for i in session.query(raw_access_read_tbl).filter(~raw_access_read_tbl.Image.like('%Everything.exe')).filter(~raw_access_read_tbl.Image.like('System')).filter(~raw_access_read_tbl.Image.like('%System32%')).filter(~raw_access_read_tbl.Image.like('%TrustedInstaller.exe')):
 	print i.ProcessID, i.Image, i.EventTime, i.Hostname
