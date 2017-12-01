501b1d2d2585d67360955f349dc6d8c6c5bccd31
yobgmin <lym9613@police.ac.kr>
2017-12-02 00:17:51 +0900

    fds

--- Morm.py
+++ Morm.py
@@ -301,7 +301,7 @@ def findParent(PrcImage, PrcId):
 			print "ParentImage : ", i.ParentImage
 			return i.ParentImage, i.ParentProcessId
 		else:
-			return "", ""
+			return "NULL", "NULL"
 
 def findChildren(PrcImage, PrcId):
 	for i in session.query(proc_tbl).filter(proc_tbl.ParnetImage.like(PrcImage)).filter(proc_tbl.ProcessID.like(PrcId)):
@@ -309,7 +309,7 @@ def findChildren(PrcImage, PrcId):
 			print "ChildImage : ", i. Image
 			return i.Image, i.ProcessID
 		else:
-			return "",""
+			return "NULL","NULL"
 
 def network_connection(PrcImage, PrcId, HstName):
 	for i in session.query(network_connection).filter(network_connection.Image.like(PrcImage)).filter(~network_connection.Hostname.like(HstName)).filter(proc_tbl.ProcessID.like(PrcId)):
@@ -317,7 +317,7 @@ def network_connection(PrcImage, PrcId, HstName):
 			print "Network Connection : ", i.EventTime, i.SourceHostname, i.DestinationHostname, i.SourceIp, i.DestinationIp
 			return i.EventTime, i.SourceHostname, i.DestinationHostname, i.SourceIp, i.DestinationIp
 		else:
-			return "", "", "", "", ""
+			return "NULL","NULL","NULL","NULL","NULL"
 
 def system_network_connection(EvtTime):
 	for i in session.query(network_connection).filter(network_connection.EventTime.like(EvtTime)).filter(~network_connection.Hostname.like(HstName)).filter(network_connection.Image.like('System')):
@@ -325,7 +325,7 @@ def system_network_connection(EvtTime):
 			print "System Network Connect", i.EventTime, i.SourceHostname, i.DestinationHostname, i.SourceIp, i.DestinationIp
 			return i.EventTime, i.SourceHostname, i.DestinationHostname, i.SourceIp, i.DestinationIp
 		else:
-			return "", "", "", "", ""
+			return "NULL","NULL","NULL","NULL","NULL"
 
 def printLine():
 	print "===================================================================================================="
@@ -375,7 +375,8 @@ for i in session.query(proc_access_tbl).filter(proc_access_tbl.TargetImage.like(
 	while Img is not None:
 		PrcList.append((Img, Pid))
 		temp = findParent(Img, Pid)
-		print temp
+		Img=temp[0]
+		Pid=temp[1]
 	for prc in PrcList:
 		net_con = network_connection((prc[0], prc[1]), i.Hostname[0])
 		if net_con[0]:
