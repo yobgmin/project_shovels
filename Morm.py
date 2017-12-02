ea717073da02cda8a8e7d139b4a6f477df4955f4
yobgmin <lym9613@police.ac.kr>
2017-12-02 15:30:51 +0900

    fd

--- Morm.py
+++ Morm.py
@@ -348,8 +348,17 @@ for i in session.query(proc_tbl).filter(proc_tbl.Image.like('%cmd.exe')).filter(
 		host_process_create(i.EventTime, i.Hostname)
 	HstName = None
 
-	Img = '%'+i.ParentImage.split('\\')[-1]
-	HstName = network_connection_EventTime(i.EventTime, i.Hostname, Img) # plus minus 2 seconds
+	PrcList=[]
+	Img = i.Image
+	while:
+		Img = findParent_Image('%'+Img.split('\\')[-1], i.EventTime)
+		if Img is not "NULL":
+			PrcList.append(Img)
+		else:
+			break
+	
+	for Img in PrcList
+		HstName = network_connection_EventTime(i.EventTime, i.Hostname, Img) # plus minus 2 seconds
 
 	if HstName is not None:
 		host_process_create(i.EventTime, i.Hostname)
