6e1f40c70c6cce0c04ba657b437f248c1e2f1d91
yobgmin <lym9613@police.ac.kr>
2017-12-02 10:41:55 +0900

    fsd

--- Morm.py
+++ Morm.py
@@ -305,8 +305,11 @@ def findParent(PrcImage, PrcId):
 
 def findParent_Image(PrcImage):
 	for i in session.query(proc_tbl).filter(proc_tbl.Image.like(PrcImage)):
-		print "ParentImage : ", i.ParentImage
-		return i.ParentImage
+		if i is not None:
+			print "ParentImage : ", i.ParentImage
+			return i.ParentImage
+		else:
+			return None
 
 def findChildren(PrcImage, PrcId):
 	for i in session.query(proc_tbl).filter(proc_tbl.ParnetImage.like(PrcImage)).filter(proc_tbl.ProcessID.like(PrcId)):
