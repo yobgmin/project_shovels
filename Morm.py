25a1e34283cab6c8aea0ffc9df7af5a9c901785f
yobgmin <lym9613@police.ac.kr>
2017-12-02 11:25:01 +0900

    fd

--- Morm.py
+++ Morm.py
@@ -383,8 +383,9 @@ for i in session.query(file_create_tbl).filter(~file_create_tbl.TargetFilename.l
 
 	HstName = system_network_connection(i.EventTime, i.Hostname)
 	print HstName
-	for j in session.query(proc_tbl).filter(proc_tbl.Hostname.like(HstName[1])).filter(proc_tbl.EventTime.like(HstName[0])):
-		print "Host Process Create - ", j.Image, j.EventTime, j.Hostname, j.CommandLine
+	if HstName is not None:
+		for j in session.query(proc_tbl).filter(proc_tbl.Hostname.like(HstName[1])).filter(proc_tbl.EventTime.like(HstName[0])):
+			print "Host Process Create - ", j.Image, j.EventTime, j.Hostname, j.CommandLine
 	print '%'+i.TargetFilename.split('\\')[-1]
 	Img = findParent_Image('%'+i.TargetFilename.split('\\')[-1])
 
