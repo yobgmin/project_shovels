cbb8be4112f4118aaea56f2917f2a2e8f14248c0
yobgmin <lym9613@police.ac.kr>
2017-12-02 13:15:37 +0900

    fds

--- Morm.py
+++ Morm.py
@@ -337,7 +337,7 @@ def printLine():
 	print "===================================================================================================="
 
 def host_process_create(EvtTime, HstName):
-	for i in session.query(proc_tbl).filter(~proc_tbl.Hostname.like(HstName)).filter(proc_tbl.EventTime.between(EvtTime+timedelta(seconds=-3), EvtTime+timedelta(seconds=3))):
+	for i in session.query(proc_tbl).filter(~proc_tbl.Hostname.like(HstName)).filter(proc_tbl.EventTime.between(EvtTime+timedelta(seconds=-2), EvtTime+timedelta(seconds=2))):
 		print "Host Process Create ", i.Image, i.EventTime, i.Hostname, i.CommandLine
 
 """
