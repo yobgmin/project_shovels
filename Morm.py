7b2412d2c254f7833c7788b7e2998f6939c29672
yobgmin <lym9613@police.ac.kr>
2017-12-09 23:39:58 +0900

    sdf

--- Morm.py
+++ Morm.py
@@ -533,6 +533,6 @@ for i in session.query(sec_evt_tbl).filter(sec_evt_tbl.EventID.like('4624')).fil
 	print "Security Log - Remote", i.EventTime, i.ProcessName, i.Hostname, i.LogonType
 	if i.LogonType=='3':
 		for i in session.query(sec_evt_tbl).filter(sec_evt_tbl.EventID.like('4648')).filter(sec_evt_tbl.EventTime.between(i.EventTime+timedelta(seconds=-2), i.EventTime+timedelta(seconds=2))).filter(~sec_evt_tbl.Hostname.like(i.Hostname)):
-			print "Security Log - Source-----", i.EventTime, i.ProcessName, i.Hostname, i.LogonType
+			print "Security Log - Source-----", i.EventTime, i.ProcessName, i.Hostname, i.EventID
 		
 
